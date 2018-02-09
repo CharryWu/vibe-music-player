d6f1b1316f09a0ed208944ba0087f57db0c6ee7d
chadlohrli <chad.david.lohrli@gmail.com>
2018-02-09 11:14:55 -0800

    Revert "Merge remote-tracking branch 'origin/development' into development"
    This reverts commit 790e6763d87fb29a00c0bfcbc9c90342ae530b87, reversing
    changes made to b974f7032fb514dabe63666f5b9067713c2d101f.

--- app/src/main/java/com/example/chadlohrli/myapplication/MainActivity.java
+++ app/src/main/java/com/example/chadlohrli/myapplication/MainActivity.java
@@ -3,11 +3,11 @@ package com.example.chadlohrli.myapplication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class AlbumActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_album);
+        setContentView(R.layout.activity_main);
     }
 }

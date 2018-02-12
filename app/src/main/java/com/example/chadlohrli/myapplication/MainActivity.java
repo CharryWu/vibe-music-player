ee722e8d560c3b85995ced30b24e0309832e78ed
chadlohrli <chad.david.lohrli@gmail.com>
2018-02-11 17:46:40 -0800

    fix main activity

--- app/src/main/java/com/example/chadlohrli/myapplication/MainActivity.java
+++ app/src/main/java/com/example/chadlohrli/myapplication/MainActivity.java
@@ -40,56 +40,6 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
-        SharedPreferences pref = getSharedPreferences("initial_setup", MODE_PRIVATE);
-        Map<String, ?> setup = pref.getAll();
-        Boolean v = pref.getAll().containsKey("completedSetUp");
-        if (!v) {
-            SharedPreferences songsList = getSharedPreferences("songslist", MODE_PRIVATE);
-            SharedPreferences.Editor metaEdit = songsList.edit();
-
-            MediaMetadataRetriever mmr = new MediaMetadataRetriever();
-
-            int id;
-            int song_length;
-            String album_title;
-            String song_title;
-            String song_path;
-
-            File file = new File(R.raw.class.toString());
-            File[] list = file.listFiles();
-
-            for (File f : list) {
-                String path = f.getName();
-                mmr.setDataSource(path);
-                if (path.endsWith(".mp3")) {
-                    song_path = f.getAbsolutePath();
-
-                    album_title = mmr.extractMetadata(MediaMetadataRetriever.METADATA_KEY_ALBUM);
-                    song_title = mmr.extractMetadata(MediaMetadataRetriever.METADATA_KEY_TITLE);
-                    id = (album_title + song_title).hashCode();
-                    song_length = Integer.parseInt(mmr.extractMetadata(MediaMetadataRetriever.METADATA_KEY_DURATION));
-
-                    SharedPreferences newSong = getSharedPreferences(String.valueOf(id), MODE_PRIVATE);
-                    SharedPreferences.Editor songadd = newSong.edit();
-
-                    songadd.putFloat("Latitude", 0);
-                    songadd.putFloat("Longitude", 0);
-                    songadd.putInt("Day", 0);
-                    songadd.putInt("Time", 0);
-                    songadd.putString("Album Time", album_title);
-                    songadd.putInt("Song Length", song_length);
-                    songadd.putString("Song path", song_path);
-                    songadd.putFloat("Rating", 0);
-                    songadd.putInt("Times played", 0);
-                    songadd.putInt("Last played", 0);
-
-                    songadd.commit();
-                }
-
-            }
-            SharedPreferences.Editor ed = pref.edit();
-            ed.putString("completedSetUp", "true").apply();
-        }
     }
 }
 

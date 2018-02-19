9f570ce9609c598227c9334885467d359b479b5d
Marissa Hing <mnhing@ucsd.edu>
2018-02-18 18:52:40 -0800

    fixed not adding to flashback list if rating is disliked

--- app/src/androidTest/java/Tests/MusicPlayerTest.java
+++ app/src/androidTest/java/Tests/MusicPlayerTest.java
@@ -13,6 +13,7 @@ import com.example.chadlohrli.myapplication.MockDateHelperMorning;
 import com.example.chadlohrli.myapplication.MusicPlayer;
 import com.example.chadlohrli.myapplication.MusicService;
 import com.example.chadlohrli.myapplication.R;
+import com.example.chadlohrli.myapplication.SharedPrefs;
 import com.example.chadlohrli.myapplication.SongData;
 import com.example.chadlohrli.myapplication.SongListActivity;
 import com.example.chadlohrli.myapplication.SongParser;
@@ -38,6 +39,27 @@ import static org.junit.Assert.assertThat;
 @RunWith(AndroidJUnit4.class)
 public class MusicPlayerTest {
 
+    private ArrayList<SongData> songList = new ArrayList<SongData>();
+    private SongData song1;
+    private SongData song2;
+    private String id;
+    private float latitude;
+    private float longitude;
+    private int day;
+    private int time;
+    private int rating;
+    private int state;
+    private int timesPlayed;
+    private String lastPlayed;
+    private String rid;
+    private float rlatitude;
+    private float rlongitude;
+    private int rday;
+    private int rtime;
+    private int rrating;
+    private int rstate;
+    private int rtimesPlayed;
+    private String rlastPlayed;
     private ArrayList<SongData> songs;
     private MusicPlayer musicPlayer;
 
@@ -55,20 +77,7 @@ public class MusicPlayerTest {
        intent.putExtra("SONGS", songs);
        intent.putExtra("CUR", 0);
        musicPlayerRule.launchActivity(intent);
-
-            /**
-        songs = new ArrayList<SongData>();
-        SongData defaultSong = new SongData("mock", "mock", "mock", "mock", "mock", "mock");
-        songs.add(defaultSong);
-        Intent intent = new Intent();
-        intent.putExtra("SONGS", songs);
-        musicPlayerRule.launchActivity(intent);
-        */
-    };
-
-
-
-
+    }
 
    @Test
     public void testGetTimeOfDay() {
@@ -86,6 +95,7 @@ public class MusicPlayerTest {
         assertEquals(0, timeOfDay);
 
    }
+
    @Test
    public void testGetDay() {
         musicPlayerRule.getActivity().setDateHelper(new MockDateHelper());
@@ -99,8 +109,37 @@ public class MusicPlayerTest {
         musicPlayerRule.getActivity().setDateHelper(new MockDateHelperAfternoon());
         day = musicPlayerRule.getActivity().getDay();
         assertEquals(Calendar.SUNDAY, day);
-
    }
 
+    @Test
+    public void testAllDiskliked() {
+        id = "everglow";
+        latitude = (float) 32.886100;
+        longitude = (float) -117.243945;
+        day = Calendar.MONDAY;
+        time = 4;
+        rating = 2;
+        state = 0;
+        timesPlayed = 3;
+        lastPlayed = "1995.12.21.21.12.32";
+        rid = "hymnfortheweekend";
+        rlatitude = (float) 32.927315;
+        rlongitude = (float) -117.102829;
+        rday = Calendar.MONDAY;
+        rtime = 4;
+        rrating = 5;
+        rstate = 0;
+        rtimesPlayed = 3;
+        rlastPlayed = "1995.12.21.21.12.32";
+
+        song1 = new SongData(id, "10", "lala", "everglow", "coldplay", "a");
+        song2 = new SongData(rid, "12", "lala2", "happier", "ed", "b");
+
+        SharedPrefs.saveData(musicPlayerRule.getActivity().getApplicationContext(),id,latitude,longitude,day,time,rating,state,timesPlayed, lastPlayed);
+        SharedPrefs.saveData(musicPlayerRule.getActivity().getApplicationContext(),rid, rlatitude,rlongitude,rday,rtime,rrating,rstate,rtimesPlayed, rlastPlayed);
+
+        songList.add(song1);
+        songList.add(song2);
+    }
 
 }

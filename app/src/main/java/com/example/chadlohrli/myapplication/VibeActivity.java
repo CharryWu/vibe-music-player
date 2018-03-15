05f303feda98af9a30b5b9809fe76198c754cdb7
thunderkar <31988938+thunderkar@users.noreply.github.com>
2018-03-14 18:27:43 -0700

    vibe unique works

--- app/src/main/java/com/example/chadlohrli/myapplication/VibeActivity.java
+++ app/src/main/java/com/example/chadlohrli/myapplication/VibeActivity.java
@@ -53,6 +53,8 @@ public class VibeActivity extends AppCompatActivity {
     private ArrayList<SongData> vibeSongs = new ArrayList<SongData>();
     private Set<SongData> setSong;
     private ArrayList<SongData> vibeFinalPlaylist;
+    private boolean friendFired = false;
+    private boolean songFired = false;
 
     /*
     private Set<String> set;
@@ -73,7 +75,7 @@ public class VibeActivity extends AppCompatActivity {
     }
 
     public int matchWeek(String songTimestamp) {
-        Log.i("Timestampforsong", songTimestamp);
+        Log.d("A Timestampforsong", songTimestamp);
         Date curtime = Calendar.getInstance().getTime();
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss");
         Date songTime = null;
@@ -91,11 +93,13 @@ public class VibeActivity extends AppCompatActivity {
     }
 
     public double matchLocation(Location songLoc) {
-        Log.i("Lat", String.valueOf(songLoc.getLatitude()));
-        Log.i("Long", String.valueOf(songLoc.getLongitude()));
+        Log.d("A Lat for song", String.valueOf(songLoc.getLatitude()));
+        Log.d("A Long for song", String.valueOf(songLoc.getLongitude()));
 
         double distance = songLoc.distanceTo(location);
         double locRating = 0;
+
+        Log.d("A Distance of song from phone", String.valueOf(distance));
         if (distance <= 304.8) {
             locRating += 2;
         }
@@ -114,6 +118,8 @@ public class VibeActivity extends AppCompatActivity {
 
         Location loc = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
 
+        Log.d("Phone Lat", String.valueOf(loc.getLatitude()));
+        Log.d("Phone Long", String.valueOf(loc.getLongitude()));
         if (loc != null) {
             return loc;
         } else {
@@ -133,15 +139,16 @@ public class VibeActivity extends AppCompatActivity {
     protected void vibe(){
         location = getLoc();
         mAuth = FirebaseAuth.getInstance();
-        vibeSongs = createDownloadedSongs();
+        //vibeSongs = createDownloadedSongs();
         myRef.child("songs").addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
+                songFired = true;
                 for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
                     String lp = snapshot.child("lastPlayed").getValue(String.class);
                     //int newR = snapshot.child("rating").getValue(int.class) + matchWeek(lp);
                     //snapshot.child("rating").getRef().setValue(newR);
-                    Log.i("Song current",snapshot.getKey());
+                    Log.d("A Song current",snapshot.getKey());
                     // next 3 uncomment
                     int wR = matchWeek(lp);
                     SharedPrefs.updateRating(VibeActivity.this.getApplicationContext(), snapshot.getKey(), wR);
@@ -180,6 +187,9 @@ public class VibeActivity extends AppCompatActivity {
                         //vibeListURLs.add(snapshot.child("url").getValue(String.class));
                     }
                 }
+                if(friendFired && songFired){
+                    makePlaylist();
+                }
             }
 
             @Override
@@ -190,24 +200,25 @@ public class VibeActivity extends AppCompatActivity {
         //get CURRENT USER ID HERE
         FirebaseUser currentUser = mAuth.getCurrentUser();
         final String curId = currentUser.getUid();
-        Log.i("ME curr", curId);
+        Log.d("A ME curr", curId);
         myRef.child("users").addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
+                friendFired = true;
                 for (DataSnapshot snapshot : dataSnapshot.child(curId).child("friends").getChildren()) {
                     String friendid = snapshot.getKey();
-                    Log.i("Friend curr", snapshot.getKey());
+                    Log.d("A Friend curr", snapshot.getKey());
                     for(DataSnapshot fSongs: dataSnapshot.child(friendid).child("songs").getChildren()){
                         // GET ORIGINAL RATING IN NEWR
                         // int newR = myRef.child("songs").child(snapshot.getKey()).child("rating")
                         // CHANGE RATINGS BY ADDING 2
-                        Log.i("User sonng curr", fSongs.getKey());
+                        Log.d("A User song curr", fSongs.getKey());
                         SharedPreferences pref = getSharedPreferences(fSongs.getKey(), MODE_PRIVATE);
                         int curRate = pref.getInt("Rating", 0);
                         SharedPrefs.updateRating(VibeActivity.this.getApplicationContext(),
                                 snapshot.getKey(),curRate + 2);
                         SharedPrefs.updateFriendPlayed(VibeActivity.this.getApplicationContext(), snapshot.getKey(), 2);
-                        SongData song = new SongData(snapshot.getKey(), null, null, null,
+                        SongData song = new SongData(fSongs.getKey(), null, null, null,
                                 null, null, snapshot.child("url").getValue(String.class));
                         boolean state = pref.getBoolean("downloaded", false);
                         if(!state) {
@@ -215,6 +226,9 @@ public class VibeActivity extends AppCompatActivity {
                         }
                         //vibeList.add(snapshot.getKey());
                         //vibeListURLs.add(snapshot.child("url").getValue(String.class));
+                        if(friendFired && songFired){
+                            makePlaylist();
+                        }
                     }
                 }
             }
@@ -223,7 +237,10 @@ public class VibeActivity extends AppCompatActivity {
             public void onCancelled(DatabaseError databaseError) {
             }
         });
+    }
 
+    protected void makePlaylist(){
+        Log.d("Listener done", "well its done");
         // Get unique song ids only
         /*set = new HashSet<String>(vibeList);
         finalRec = new ArrayList<String>(set);
@@ -231,13 +248,16 @@ public class VibeActivity extends AppCompatActivity {
         seturl = new HashSet<String>(vibeListURLs);
         finalRecURL = new ArrayList<String>(seturl);*/
 
+        Log.d("Vibe song playlist before uniquing", String.valueOf(vibeSongs.size()));
         setSong = new HashSet<SongData>(vibeSongs);
         vibeFinalPlaylist = new ArrayList<SongData>(setSong);
 
         Collections.sort(vibeFinalPlaylist, new VibeSongSorter(getApplicationContext()));
 
+        Log.d("Vibe playlist size", String.valueOf(vibeFinalPlaylist.size()));
         for(int i = 1; i <= vibeFinalPlaylist.size(); i++){
             vibeFinalPlaylist.get(i - 1).setPriority(i);
+            Log.d("Vibe playlist", vibeFinalPlaylist.get(i - 1).getID());
         }
 
         //PASS finalRecURL to Download Service and start downloads
@@ -259,8 +279,8 @@ public class VibeActivity extends AppCompatActivity {
                 finish();
             }
         });
-
     }
+
     public ArrayList<SongData> createDownloadedSongs() {
         File musicDirectory = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_MUSIC);
         File[] fields = musicDirectory.listFiles();

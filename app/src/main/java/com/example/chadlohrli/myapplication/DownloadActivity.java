d5b8b64f750dbe611d84e3d47bbd7e098879b08e
kyle <k1burt@ucsd.edu>
2018-03-16 08:48:56 -0700

    need album download

--- app/src/main/java/com/example/chadlohrli/myapplication/DownloadActivity.java
+++ app/src/main/java/com/example/chadlohrli/myapplication/DownloadActivity.java
@@ -6,6 +6,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.SharedPreferences;
+import android.database.Cursor;
 import android.net.Uri;
 import android.os.Environment;
 import android.support.annotation.NonNull;
@@ -22,11 +23,14 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.io.Writer;
 import java.util.UUID;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 
 public class DownloadActivity extends AppCompatActivity {
     private BottomNavigationView bottomNav;
@@ -35,6 +39,7 @@ public class DownloadActivity extends AppCompatActivity {
     private String[] addressArray;
     private EditText editText;
     private Button downloadButton;
+    private Button downloadAlbumButton;
     private String id;
 
 
@@ -47,12 +52,38 @@ public class DownloadActivity extends AppCompatActivity {
             toast.setGravity(Gravity.TOP, 25, 400);
             toast.show();
 
-            File musicDirectory = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_MUSIC);
-            Log.v("Files",musicDirectory.exists()+"");
-            Log.v("Files",musicDirectory.isDirectory()+"");
-            Log.v("Files",musicDirectory.listFiles()+"");
-            File[] files = musicDirectory.listFiles();
-            int s = files.length;
+            long referenceId = intent.getLongExtra(DownloadManager.EXTRA_DOWNLOAD_ID, -1);
+            //directory that song has been stored in
+            String path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_MUSIC).getAbsolutePath();
+
+
+            DownloadManager.Query query = new DownloadManager.Query();
+            query.setFilterById(referenceId);
+            DownloadManager downloadManager = (DownloadManager) context.getSystemService(Context.DOWNLOAD_SERVICE);
+            Cursor cursor = downloadManager.query(query);
+
+            if (cursor.moveToFirst()) ;
+            {
+                //get description of download which is id if song was zip
+                String id = cursor.getString(cursor.getColumnIndex(DownloadManager.COLUMN_DESCRIPTION));
+
+
+                //get title of column which is "zip" if file was zip file
+                String typeOfDownload = cursor.getString(cursor.getColumnIndex(DownloadManager.COLUMN_TITLE));
+                if (typeOfDownload == "zip") {
+                    try {
+                        unzip(id);
+                    }
+                    catch (Exception e) {
+                        Log.e("zip doesnt work", "zip");
+                    }
+
+                }
+                //Log.d("songId", songId);
+
+
+            }
+
 
             //renameSong();
 
@@ -68,6 +99,7 @@ public class DownloadActivity extends AppCompatActivity {
         bottomNav = (BottomNavigationView) findViewById(R.id.navigation);
         editText = (EditText) findViewById(R.id.download_form);
         downloadButton = (Button) findViewById(R.id.download_button);
+        downloadAlbumButton = (Button) findViewById(R.id.download_album_button);
 
         bottomNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
@@ -90,7 +122,15 @@ public class DownloadActivity extends AppCompatActivity {
             }
         });
 
-        downloadManager = (DownloadManager)getSystemService(DOWNLOAD_SERVICE);
+        downloadAlbumButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String url = editText.getText().toString();
+                downloadAlbum(url);
+            }
+        });
+
+        downloadManager = (DownloadManager) getSystemService(DOWNLOAD_SERVICE);
         registerReceiver(onComplete, new IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE));
 
 
@@ -108,9 +148,9 @@ public class DownloadActivity extends AppCompatActivity {
         //http://soundbible.com/grab.php?id=2200&type=mp3
         //http://soundbible.com/grab.php?id=2190&type=mp3
         File musicDirectory = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_MUSIC);
-        Log.v("Files",musicDirectory.exists()+"");
-        Log.v("Files",musicDirectory.isDirectory()+"");
-        Log.v("Files",musicDirectory.listFiles()+"");
+        Log.v("Files", musicDirectory.exists() + "");
+        Log.v("Files", musicDirectory.isDirectory() + "");
+        Log.v("Files", musicDirectory.listFiles() + "");
         File[] files = musicDirectory.listFiles();
         int s = files.length;
 
@@ -119,7 +159,7 @@ public class DownloadActivity extends AppCompatActivity {
         Uri uri = Uri.parse(url);
         id = String.valueOf(url.hashCode());
 
-        try{
+        try {
 
             DownloadManager.Request request = new DownloadManager.Request(uri);
             request.setTitle(id);
@@ -136,10 +176,10 @@ public class DownloadActivity extends AppCompatActivity {
 
 
             //save url for later pushing to firebase
-            SharedPrefs.updateURL(this,id,uri.toString());
+            SharedPrefs.updateURL(this, id, uri.toString());
 
 
-        }catch(IllegalArgumentException e){
+        } catch (IllegalArgumentException e) {
 
             Toast toast = Toast.makeText(DownloadActivity.this,
                     "Invalid URL", Toast.LENGTH_LONG);
@@ -150,13 +190,48 @@ public class DownloadActivity extends AppCompatActivity {
 
     }
 
-    public void renameSong(){
+    public void downloadAlbum(String url) {
+        Uri uri = Uri.parse(url);
+        id = String.valueOf(url.hashCode());
+
+        try {
+
+            DownloadManager.Request request = new DownloadManager.Request(uri);
+            request.setTitle("zip");
+
+            request.setDescription(id);
+
+            request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, id);
+
+
+            request.setMimeType("df.zip");
+
+            downloadManager.enqueue(request);
+
+
+            //save url for later pushing to firebase
+            //SharedPrefs.updateURL(this, id, uri.toString());
+
+
+        } catch (IllegalArgumentException e) {
+
+            Toast toast = Toast.makeText(DownloadActivity.this,
+                    "Invalid URL", Toast.LENGTH_LONG);
+            toast.setGravity(Gravity.TOP, 25, 400);
+            toast.show();
+
+        }
+
+    }
+
+
+    public void renameSong() {
 
         String path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_MUSIC).getAbsolutePath();
 
-        SongData song = SongParser.parseSong(path,id,this);
+        SongData song = SongParser.parseSong(path, id, this);
 
-        Log.i("PREV SONG ID",song.getID());
+        Log.i("PREV SONG ID", song.getID());
 
 
         String newId = (song.getAlbum() + song.getTitle()
@@ -165,7 +240,7 @@ public class DownloadActivity extends AppCompatActivity {
 
         newId = String.valueOf(newId.hashCode());
 
-        Log.i("NEW SONG ID",newId);
+        Log.i("NEW SONG ID", newId);
 
         File of = new File(song.getPath());
 
@@ -180,4 +255,51 @@ public class DownloadActivity extends AppCompatActivity {
 
 
     }
+
+    public void unzip(String zipId) throws IOException {
+        String downloadDirectoryPath = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).getAbsolutePath();
+        String zipFilePath = downloadDirectoryPath+zipId;
+
+        String songDirectory = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_MUSIC).getAbsolutePath();
+
+        try {
+            File f = new File(songDirectory);
+            if(!f.isDirectory()) {
+                f.mkdirs();
+            }
+            ZipInputStream zin = new ZipInputStream(new FileInputStream(zipFilePath));
+            try {
+                ZipEntry ze = null;
+                while ((ze = zin.getNextEntry()) != null) {
+                    String path = songDirectory + ze.getName();
+
+                    if (ze.isDirectory()) {
+                        File unzipFile = new File(path);
+                        if(!unzipFile.isDirectory()) {
+                            unzipFile.mkdirs();
+                        }
+                    }
+                    else {
+                        FileOutputStream fout = new FileOutputStream(path, false);
+                        try {
+                            for (int c = zin.read(); c != -1; c = zin.read()) {
+                                fout.write(c);
+                            }
+                            zin.closeEntry();
+                        }
+                        finally {
+                            fout.close();
+                        }
+                    }
+                }
+            }
+            finally {
+                zin.close();
+            }
+        }
+        catch (Exception e) {
+            Log.e("Unzipping problem", "Unzip exception", e);
+        }
+    }
+
 }
